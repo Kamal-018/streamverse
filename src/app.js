fc4b1c71b3c2b6ad4fd7b0f0b1289c728383843d
Kamal Khatri <khatrikamal2001@gmail.com>
2026-02-06 00:31:38 +0545

    Fix cross-site cookies and increase payload limits

--- src/app.js
+++ src/app.js
@@ -33,9 +33,9 @@ app.get("/api/v1/health", (req, res) => {
 });
 
 //limiter
-app.use(express.json({ limit: "16kb" }))
+app.use(express.json({ limit: "50mb" }))
 
-app.use(express.urlencoded({ Extended: true, limit: "16kb" }))
+app.use(express.urlencoded({ Extended: true, limit: "50mb" }))
 
 //to store temp data
 app.use(express.static("public"))
